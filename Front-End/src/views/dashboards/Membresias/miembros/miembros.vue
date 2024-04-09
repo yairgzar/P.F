d8d53e5d59a0d5c99df566d22efc14ef9d13f6ef
hugo <hugo.sago30@gmail.com>
2024-04-09 02:10:33 -0600

    se creo el archivo miembros

---
+++ Front-End/src/views/dashboards/Membresias/miembros/miembros.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+// common components
+import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
+import EditableTable from '@/components/table/EditableTable.vue';
+// theme breadcrumb
+const page = ref({ title: 'Lista de miembros' });
+
+const breadcrumbs = ref([
+    {
+        text: '',
+        disabled: true,
+        href: '#'
+    }
+]);
+</script>
+
+<template>
+    <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
+    <v-card elevation="10">
+        <v-card-text>
+            <EditableTable />
+        </v-card-text>
+    </v-card>
+</template>
