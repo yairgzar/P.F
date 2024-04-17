b823ca14c75ba5e921b2341cf959b1088dd6e7a1
Hugosago <hugo.sago30@gmail.com>
2024-04-16 23:57:29 -0600

    se creo el archivo urls

---
+++ BackendGym/myapi/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path,include
+from rest_framework import routers
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'tbl_cliente', views.tbl_clienteViewSet)
+router.register(r'tbl_rol', views.tbl_rolViewSet)
+router.register(r'tb_membresias', views.tb_membresiasViewSet)
+router.register(r'tb_personas', views.tb_personasViewSet)
+router.register(r'tb_miembros', views.tb_miembrosViewSet)
+router.register(r'tb_usuarios', views.tb_usuariosViewSet)
+router.register(r'tb_membresias_usuarios', views.tb_membresias_usuariosViewSet)
+
+urlpatterns = [
+	path('api/v1',include(router.urls))
+]
