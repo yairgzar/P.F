c15780ae2c3b7e4626778ff0184086e19960b0c4
Hugosago <hugo.sago30@gmail.com>
2024-04-17 19:58:48 -0600

    modificacion de las views para la consulta de los objetos de los modelos

--- BackendGym/myapi/views.py
+++ BackendGym/myapi/views.py
@@ -1,15 +1,15 @@
 from rest_framework import viewsets
-from .models import tb_membresias, tb_membresias_usuarios, tb_miembros, tb_personas, tb_usuarios
+from .models import tb_membresias, tb_personas, tb_miembros,  tb_usuarios, tb_membresias_usuarios
 from .serializer import  tb_membresiasSerializer, tb_personasSerializer, tb_miembrosSerializer, tb_usuariosSerializer, tb_membresias_usuariosSerializer
 
 
-
+ 
 class tb_membresiasViewSet(viewsets.ModelViewSet):
 	queryset = tb_membresias.objects.all()
 	serializer_class = tb_membresiasSerializer
 
 class tb_personasViewSet(viewsets.ModelViewSet):
-	queryset = tb_membresias_usuarios.objects.all()
+	queryset = tb_personas.objects.all()
 	serializer_class = tb_personasSerializer
 
 class tb_miembrosViewSet(viewsets.ModelViewSet):
@@ -17,7 +17,7 @@ class tb_miembrosViewSet(viewsets.ModelViewSet):
 	serializer_class = tb_miembrosSerializer
 
 class tb_usuariosViewSet(viewsets.ModelViewSet):
-	queryset = tb_personas.objects.all()
+	queryset = tb_usuarios.objects.all()
 	serializer_class = tb_usuariosSerializer
 
 class tb_membresias_usuariosViewSet(viewsets.ModelViewSet):
