1ca62bb59627380b31a18ff2b041dcb1592644cf
Hugosago <hugo.sago30@gmail.com>
2024-04-17 20:00:32 -0600

    se modifico el serializer

--- BackendGym/myapi/serializer.py
+++ BackendGym/myapi/serializer.py
@@ -1,37 +1,28 @@
 from rest_framework import serializers
-from .models import tbl_cliente,tbl_rol, tb_membresias, tb_personas, tb_miembros, tb_usuarios, tb_membresias_usuarios
+from .models import  tb_membresias, tb_personas, tb_miembros, tb_usuarios, tb_membresias_usuarios
 
-class tbl_clienteSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = tbl_cliente
-		fields = '_all_'
-		
-class tbl_rolSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = tbl_rol
-		fields = '_all_'
 
 class tb_membresiasSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = tb_membresias
-		fields = '_all_'
+		fields = '__all__'
 
 class tb_personasSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = tb_personas
-		fields = '_all_'
+		fields = '__all__'
 
 class tb_miembrosSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = tb_miembros
-		fields = '_all_'
+		fields = '__all__'
 
 class tb_usuariosSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = tb_usuarios
-		fields = '_all_'
+		fields = '__all__'
 
 class tb_membresias_usuariosSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = tb_membresias_usuarios
-		fields = '_all_'
+		fields = '__all__'
